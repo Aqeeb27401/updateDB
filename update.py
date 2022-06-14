c1cab813b24ecf012026a1d56d2ae626cd49bb9b
Aqeeb Hussain <77690409+Aqeeb27401@users.noreply.github.com>
2022-06-14 11:09:05 +0500

    Update update.py
    update database by taking data from user

--- update.py
+++ update.py
@@ -1,6 +1,8 @@
 import mysql.connector as c
 con=c.connect(host="localhost",user="root",passwd="",database="aqeeb")
 Cursor=con.cursor()
-Cursor.execute("UPDATE `emp` SET `salary` = '75' WHERE `emp`.`code` = 7");
+code=int(input("Enter code:"))
+salary=int(input("Enter salary:"))
+Cursor.execute("UPDATE `emp` SET `salary` = {} WHERE `emp`.`code` = {};".format(salary,code))
 con.commit()
 print("Sucessfully updated")
